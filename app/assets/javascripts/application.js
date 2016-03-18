b8e1938a7b5cbe164438170b2354483a465ce00b
Daniel Dumycz <daniel.dumycz@outlook.com>
2016-03-18 11:18:43 +1100

    Added materialize

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,4 +13,8 @@
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
-//= require_tree .
+//= require_tree
+//= require jquery
+//= require materialize-sprockets
+//= require materialize/extras/nouislider
+
